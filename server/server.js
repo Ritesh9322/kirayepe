7b96b92187ec66f7f30c31efb73643fca82663cf
Ritesh9322 <riteshtekmale@outlook.com>
2025-07-12 15:28:29 +0530

    Added full React frontend (client folder) and updated backend

--- server/server.js
+++ server/server.js
@@ -18,17 +18,29 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// Connect to MongoDB
+// Connect to MongoDBnode server.js
+
+
+// mongoose
+//   .connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("✅ MongoDB Connected Successfully"))
+//   .catch((err) => console.error("❌ MongoDB Connection Failed:", err));
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected Successfully"))
   .catch((err) => console.error("❌ MongoDB Connection Failed:", err));
+
+
 
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected Successfully"))
+  .catch((err) => console.error("❌ MongoDB Connection Failed:", err));
